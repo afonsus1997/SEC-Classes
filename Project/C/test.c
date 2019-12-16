20ef58eab1ffd01071e037c1aea5666b20c14822
Afonso Muralha <afonso.muralha@tecnico.ulisboa.pt>
2019-12-16 16:01:38 +0000

    Project improvements, edited report figures

--- Project/C/test.c
+++ Project/C/test.c
@@ -1,10 +1,31 @@
 
+#define MILSPERCLOCK 32768
+
+int loopAddr = 604;
+int pushAddr = 602;
+
+void delay(int miliseconds){
+	int y;
+	y = miliseconds*MILSPERCLOCK;
+	while(y--);
+}
 
 
 void main(){
+	
+	int * speed = (int*)loopAddr;
+	int * push = (int*)pushAddr;
 
-	int * speed = (int*)0x25C;
+	
 
-	*speed = 5;
+
+	while(1){
+		if(*push == 0){
+			*speed = 6;			
+		}
+		else{
+			*speed = 1;		
+		}
+	}
 	return;
 }
