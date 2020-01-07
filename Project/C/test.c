e70fcd9082e111435becf790c4c17ad77ff2651c
Afonso Muralha <afonso.muralha@tecnico.ulisboa.pt>
2020-01-07 20:33:32 +0000

    Started implementing buttons (reading and setting working)

--- Project/C/test.c
+++ Project/C/test.c
@@ -1,31 +1,58 @@
 
-#define MILSPERCLOCK 32768
+//#define MILSPERCLOCK 32768
+// #define deboucetm 65536
+#define deboucetm 1048576
 
+
+long debouncevar;
 int loopAddr = 604;
 int pushAddr = 602;
 
-void delay(int miliseconds){
-	int y;
-	y = miliseconds*MILSPERCLOCK;
-	while(y--);
+void debouce(long ticks){
+	
+	debouncevar = ticks;
+	while(debouncevar--);
 }
 
 
-void main(){
+main(){
 	
-	int * speed = (int*)loopAddr;
-	int * push = (int*)pushAddr;
-
-	
-
+	char * speed = (char*)loopAddr;
+	char * push = (char*)pushAddr;
+	char speedToWr = 0;
+	char oldspeed = 0;
+	char lastpush = 0;
+	*speed = 0;
 
 	while(1){
-		if(*push == 0){
+		lastpush = *push; 
+		
+		if(lastpush == 1){
+			if(speedToWr == 7)
+				speedToWr = 0;
+			else
+				speedToWr = 7;
+			//else *speed = *speed++;
+		}
+		else if(lastpush == 2)
+			speedToWr = 7;
+		else if(lastpush == 4)
+		speedToWr = 0;
+
+		if(oldspeed != speedToWr){
+			*speed = speedToWr;
+			oldspeed = speedToWr;
+		}
+		debouce(deboucetm);
+		
+	}	
+		//return;
+		/*else if(*push == 1){
 			*speed = 6;			
-		}
-		else{
-			*speed = 1;		
-		}
-	}
-	return;
+		}*/
+
+		while(1){}
+	
+	return 0;
+	
 }
